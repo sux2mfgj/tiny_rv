97c6360fc922588680317f1d8d3e5610a5f44ca2
Shunsuke Mie <sux2mfgj@gmail.com>
2018-04-10 00:00:59 +0900

    [WIP] JAL

--- main.s
+++ main.s
@@ -1,7 +1,9 @@
 
 .text
 _start:
-addi a0, a0, 2
-addi a1, a0, 0
-addi a2, a0, 3
-add a3, a0, a2
+    addi a0, a0, 2
+    addi a1, a0, 0
+    addi a2, a0, 3
+    add a3, a0, a2
+finish:
+    jal a4, finish
