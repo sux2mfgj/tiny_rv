a6123049c125dde668c6541c655ec691bc8ed191
Shunsuke Mie <sux2mfgj@gmail.com>
2018-04-19 23:15:43 +0900

    store WIP

--- bus_arbiter/main.s
+++ bus_arbiter/main.s
@@ -1,6 +1,8 @@
 .data
 hello:
     .word 0x12345678
+to_store:
+    .word 0x87654321
 
 .text
 .globl _start
@@ -9,5 +11,7 @@ _start:
     addi a0, a0, 1
     add a1, x0, a0
     add a2, a0, a1
+    la a5, to_store
+    sw a0, 0(a5)
 finish:
     jal a4, finish
