c5e017ef59be7a1f80997c14f030568ed02af199
Shunsuke Mie <sux2mfgj@gmail.com>
2018-06-10 17:22:53 +0900

    [RIP] system instructions

---
+++ core/opcode_system.cpp
@@ -0,0 +1,36 @@
+#include "Vopcode_system_tb.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    Verilated::commandArgs(argc, argv);
+
+    Vopcode_system_tb *mod = new Vopcode_system_tb;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    vluint64_t hcycle;
+    int result = 0;
+
+    mod->trace(tfp, 99);
+    tfp->open("opcode_system.vcd");
+
+    while(!Verilated::gotFinish())
+    {
+        hcycle++;
+        mod->m_clock = mod->m_clock ? 0 : 1;
+        mod->eval();
+        tfp->dump(hcycle * 10);
+        if(mod->error)
+        {
+            result = mod->error_num;
+            goto finish;
+        }
+    }
+finish:
+    tfp->close();
+    delete mod;
+    return result;
+}
